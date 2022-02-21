0fb1e1a60b0446c7b9ed9ba82c2194280c326f92
sin-2-x <silantuys@mail.ru>
2022-02-21 19:23:37 +0300

    Сделано приветствие, разобраться с конфигом, тк не работает на релизе

--- lab1/MessageForm.Designer.cs
+++ lab1/MessageForm.Designer.cs
@@ -29,35 +29,38 @@ namespace lab1
         /// </summary>
         private void InitializeComponent()
         {
-            this.textMessage = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // textMessage
-            // 
-            this.textMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+      this.textMessage = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // textMessage
+      // 
+      this.textMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textMessage.AutoSize = true;
-            this.textMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textMessage.Location = new System.Drawing.Point(33, 60);
-            this.textMessage.Name = "textMessage";
-            this.textMessage.Size = new System.Drawing.Size(0, 25);
-            this.textMessage.TabIndex = 0;
-            this.textMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // messageBox
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(328, 203);
-            this.Controls.Add(this.textMessage);
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "messageBox";
-            this.ShowIcon = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
-            this.ResumeLayout(false);
-            this.PerformLayout();
+      this.textMessage.AutoSize = true;
+      this.textMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.textMessage.Location = new System.Drawing.Point(33, 43);
+      this.textMessage.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+      this.textMessage.Name = "textMessage";
+      this.textMessage.Size = new System.Drawing.Size(0, 30);
+      this.textMessage.TabIndex = 0;
+      this.textMessage.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+      // 
+      // MessageForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(437, 250);
+      this.Controls.Add(this.textMessage);
+      this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.Name = "MessageForm";
+      this.ShowIcon = false;
+      this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+      this.Load += new System.EventHandler(this.MessageForm_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
 
         }
 
--- lab1/MessageForm.cs
+++ lab1/MessageForm.cs
@@ -8,16 +8,25 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace lab1
-{
-    public partial class MessageForm : Form
-    {
-        public MessageForm(int left, int top, string message)
-        {
-            InitializeComponent();
-            this.StartPosition = FormStartPosition.Manual;
-            this.Location = new Point(left - Width / 2, top - Height / 2);
-            this.textMessage.Text = message;
-        }
+namespace lab1 {
+  public partial class MessageForm : Form {
+    public MessageForm(int left, int top, string message) {
+      InitializeComponent();
+
+      this.StartPosition = FormStartPosition.Manual;
+      this.Location = new Point(left - Width / 2, top - Height / 2);
+      
+      this.textMessage.Text = message;
+      Point magin = textMessage.Location;
+
+      this.Size = new Size(magin.X*2+textMessage.Width, magin.Y * 3 + textMessage.Height);
+      this.MaximumSize = this.Size;
+      this.MinimumSize = this.Size;
+      this.FormBorderStyle = FormBorderStyle.FixedSingle;
     }
+
+    private void MessageForm_Load(object sender, EventArgs e) {
+
+    }
+  }
 }
